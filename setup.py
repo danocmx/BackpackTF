2af067d8e434aa09612f432c2c975877c980e95d
danocmx <43742709+danocmx@users.noreply.github.com>
2018-10-19 16:25:50 +0200

    just gotta make this work, bear with me

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/danocmx/BackpackTF",
-    packages=setuptools.find_packages(),
+    packages=['BackpackTF/'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
